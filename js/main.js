a3202fdfb77a277f37dd1cc78d39b57f7c0f3c11
Enge-Olate <marcioalexisolate@live.com>
2024-01-31 23:26:30 -0300

    IMC mensagem BAD

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const operandoPeso = e.target.querySelector('#peso');
+    const operandoAltura = e.target.querySelector('#altura');
+
+    const peso = Number(operandoPeso.value);
+    const altura = Number(operandoAltura.value);
+    console.log(peso, altura);
+
+    if (!peso) {
+      seta_resultado("Peso inválido!", false);
+      return;
+    } else if(!altura) {
+      seta_resultado("Altura inválido!", false);
+      return;
+    }
+    
+    
+
+    const imc = getImc(peso, altura);
+    console.log(getImc(peso, altura));
+    
+    const mensagem = `imc = ${imc}.`;
+    seta_resultado(mensagem, true);
+
+});
+
+function getImc (peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+  }
+  
+  function criaP () {
+    const p = document.createElement('p');
+    return p;
+  }
+  
+  function seta_resultado (msg, isValid) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+  
+    const p = criaP();
+  
+    if (isValid) {
+      p.classList.add('paragrafo_resultado');
+    } else {
+      p.classList.add('bad');
+    }
+  
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+  }
+
+
+
